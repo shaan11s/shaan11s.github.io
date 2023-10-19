1f209431c787632d7e2dab29dfd0545a9a48b250
shaan11s <65747263+shaan11s@users.noreply.github.com>
2023-10-19 00:24:29 -0500

    Add files via upload

---
+++ js/resume-game.js
@@ -0,0 +1,32 @@
+import MainScene from "./MainScene.js";
+import TitleScene from "./TitleScene.js";
+
+const config = {
+    width:512,
+    height:512,
+    backgroundColor: '#999999',
+    type: Phaser.AUTO,
+    parent: 'resume-game',
+    scene:[TitleScene,MainScene],
+    scale: {
+        zoom:2
+    },
+    physics: {
+        default: 'matter',
+        matter: {
+            debug:false,
+            gravity:{y:0},
+        }
+    },
+    plugins: {
+        scene:[
+            {
+            plugin: PhaserMatterCollisionPlugin,
+            key: 'matterCollision',
+            mapping: 'matterCollision',
+            }
+        ]
+    }
+}
+
+new Phaser.Game(config);
